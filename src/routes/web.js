3e2934119812ec5ca617c9a7d4a77fd2619de365
THAI LAM HOANG <lamhoang20212@gmail.com>
2023-09-14 10:20:41 +0700

    Update

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,9 +1,10 @@
 import express from "express";
+import homeController from "../controller/homeController";
 const router = express.Router();
+
 const initWebRoutes = (app) => {
-    router.get("/", (req, res) => {
-        return res.send("Hello world");
-    })
+    router.get("/", homeController.handleHelloWorld);
+    router.get("/user", homeController.handleUserPage);
     return app.use("/", router);
 }
 export default initWebRoutes;
