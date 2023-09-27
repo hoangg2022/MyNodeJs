e932cf4a0c7621b743550875ae87a021ec7ca41e
THAI LAM HOANG <lamhoang20212@gmail.com>
2023-09-28 00:06:53 +0700

    create table users

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -2,15 +2,15 @@ import userService from '../service/userService';
 const handleHelloWorld = (req, res) => {
     return res.render("home.ejs");
 }
-const handleUserPage = (req, res) => {
-    return res.render("user.ejs");
+const handleUserPage = async (req, res) => {
+    let userList = await userService.getUserList();
+    return res.render("user.ejs", { userList });
 }
 const handleCreateNewUser = (req, res) => {
     let email = req.body.email;
     let password = req.body.password;
     let username = req.body.username;
-    userService.createNewUser(email, password, username);
-
+    //userService.createNewUser(email, password, username);
     return res.send("handleCreateNewUser");
 }
 module.exports = {
--- src/service/userService.js
+++ src/service/userService.js
@@ -1,13 +1,13 @@
 import bcrypt from 'bcryptjs';
 // get the client
-import mysql from 'mysql2';
+import mysql from 'mysql2/promise';
+import bluebird from 'bluebird';
+
+
+// create the connection, specify bluebird as Promise
 
 // create the connection to database
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'jwt'
-});
+
 const salt = bcrypt.genSaltSync(10);
 
 const hashUserPassword = (userPassword) => {
@@ -26,6 +26,31 @@ const createNewUser = (email, password, username) => {
         }
     );
 }
+const getUserList = async () => {
+    const connection = await mysql.createConnection({ host: 'localhost', user: 'root', database: 'jwt', Promise: bluebird });
+
+    let users = [];
+    // connection.query(
+    //     'Select * from users',
+    //     function (err, results, fields) {
+    //         if (err) {
+    //             console.log(err);
+    //             return users;
+    //         }
+    //         users = results;
+    //         console.log(">>> get : ", users);
+    //         return users;
+    //     }
+    // );
+    try {
+        const [rows, fields] = await connection.execute('Select * from users');
+        return rows;
+
+    } catch (error) {
+        console.log(">>>check error: ", error);
+    }
+
+}
 module.exports = {
-    createNewUser
+    createNewUser, getUserList
 }
