e633c6cd5bbc1654237cfde31b5c0bc0adbe426b
THAI LAM HOANG <lamhoang20212@gmail.com>
2023-10-03 18:24:43 +0700

    add sequelize

--- src/server.js
+++ src/server.js
@@ -3,11 +3,13 @@ require("dotenv").config();
 import configViewEngine from "./config/viewEngine";
 import initWebRoutes from "./routes/web";
 import bodyParser from "body-parser";
+import connection from "./config/connectDB";
 const app = express();
 const PORT = process.env.PORT || 8080;
 configViewEngine(app);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+connection();
 initWebRoutes(app);
 
 app.listen(PORT, () => {
